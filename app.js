9537f81c89121114b58c260c4a9e51a8d9fbe9d0
BeChanh <lecam83it@gmail.com>
2018-06-26 17:13:39 +0700

    Config app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+// import router
+var indexRouter = require('./routers/index');
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+//set morgan to log
+app.use(logger('dev'));
+//set body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+//set view engine
+app.set('/views', express.static(__dirname + "/views"));
+app.set('view engine', 'ejs');
+//set assest
+app.use('/public', express.static(__dirname + '/public'));
+
+// router
+app.use('/', indexRouter);
+
+app.listen(PORT, function () {
+    console.log(`App listening on PORT: ${PORT}`);
+})
